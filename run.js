25b0967a993b1c0a535887bb6a29193610e4c387
John Grib <johngrib82@gmail.com>
2019-02-09 10:30:07 +0900

    Let the sample solution run

--- run.js
+++ run.js
@@ -5,7 +5,7 @@ const {
     Builder,
     By,
     Key,
-    until
+    until,
 } = require('selenium-webdriver');
 
 const inputPath = './solutions/' + process.argv.slice(2).pop() + '.js';
@@ -18,15 +18,44 @@ if (!fs.existsSync(inputPath)) {
 const solution = fs.readFileSync(inputPath, 'utf8');
 console.log(solution);
 
-return;
-
-
 const driver = new Builder()
     .forBrowser('chrome')
     .build();
 
-driver.get('http://www.google.com/ncr')
-    .then(_ =>
-        driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN))
-    .then(_ => driver.wait(until.titleIs('webdriver - Google Search'), 1000))
-    .then(_ => driver.quit());
+let time = 0;
+
+driver.get('https://play.elevatorsaga.com/')
+    .then(getTimeLimit)
+    .then(prepareInject)
+    .then(injectSolution)
+    .then(clickReset)
+    .then(clickApply);
+
+function getTimeLimit() {
+    driver
+        .findElement(By.xpath('/html/body/div/div[2]/div/h3/span[2]'))
+        .getText()
+        .then((text) => {
+            time = parseInt(text, 10);
+            console.log(text);
+            console.log(time);
+        });
+}
+
+function prepareInject() {
+    driver.executeScript('console.log("code injection start")')
+    driver.executeScript('confirm = function() { return true; };')
+    driver.executeScript("mycode = \`" + solution + "\`")
+}
+
+function injectSolution() {
+    driver.executeScript(`$('#default-elev-implementation').text(mycode)`);
+}
+
+function clickReset() {
+    driver.findElement(By.xpath('//*[@id="button_reset"]')).click();
+}
+
+function clickApply() {
+    driver.findElement(By.xpath('//*[@id="button_apply"]')).click();
+}
